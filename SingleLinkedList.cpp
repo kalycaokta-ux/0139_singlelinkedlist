2a28c8f634eacbdfc340a8bd2b3ab80623d5faf0
kalycaokta-ux <kalycaokta@gmail.com>
2026-04-23 11:45:16 +0700

    masukkan list kosong

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -79,4 +79,27 @@ public:
     }
 
     bool deNode(int nim)
+    {
+        Node *cuurent, *previous;
+
+        if (!search(nim, previous, current))
+        return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }   
+        else
+    }
 }
