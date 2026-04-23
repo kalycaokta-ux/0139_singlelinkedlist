8c07e4a9bee9c140543a307e4789fe57ca00bea2
kalycaokta-ux <kalycaokta@gmail.com>
2026-04-23 11:39:20 +0700

    menambahkan bool deNode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -60,4 +60,23 @@ public:
     }
 
     bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool deNode(int nim)
 }
